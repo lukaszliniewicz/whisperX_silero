e5f0da7aa82675d057afeb4e3e09677025dc699a
Lukasz Liniewicz <75737665+lukaszliniewicz@users.noreply.github.com>
2024-11-03 06:30:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import pkg_resources
 from setuptools import find_packages, setup
 
 setup(
-    name="whisperx",
+    name="whisperx_silero",
     py_modules=["whisperx"],
     version="3.1.1",
-    description="Time-Accurate Automatic Speech Recognition using Whisper.",
+    description="Time-Accurate Automatic Speech Recognition using Whisper and Silero VAD.",
     readme="README.md",
     python_requires=">=3.8",
-    author="Max Bain",
-    url="https://github.com/m-bain/whisperx",
+    author="Lukasz Liniewicz",
+    url="https://github.com/lukaszliniewicz/whisperX_silero",
     license="MIT",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
@@ -21,7 +21,6 @@ setup(
             open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
         )
     ]
-    + [f"pyannote.audio==3.1.1"],
     entry_points={
         "console_scripts": ["whisperx=whisperx.transcribe:cli"],
     },
